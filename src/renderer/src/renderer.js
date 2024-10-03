35238effbb1e178273c95f842e5ca96e4a38d19c
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-10-03 21:02:23 +0800

    test: successful db use case

--- src/renderer/src/renderer.js
+++ src/renderer/src/renderer.js
@@ -1,1 +1,15 @@
-import database from './database.js'
+const contactData = {
+    name: 'Benjamin Blahaj',
+    timezone: '+6',
+    location: 'Vienna, Austria',
+    details: [
+        'Had both his front teeth knocked out, those are actually his upper canines',
+        'Was held back a grade',
+        'Is very into video games'
+    ]
+}
+
+const contactId = window.api.addContact(contactData)
+const retrievedContact = JSON.stringify(window.api.getContact(contactId))
+
+console.log(`ID: ${contactId}\nDATA: ${retrievedContact}`)
