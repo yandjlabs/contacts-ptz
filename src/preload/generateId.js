749735c8140b2099f4898294c76bf7be6082346c
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-10-04 11:40:37 +0800

    feat: add check for unique id

--- src/preload/generateId.js
+++ src/preload/generateId.js
@@ -1,7 +1,19 @@
 function generateId() {
-    const idArray = Array.from({length: 4}, () => Math.floor(Math.random() * 10));
-    const id = idArray.join('')
-    return id;
+    let id
+    do {
+        const idArray = Array.from({ length: 4 }, () => Math.floor(Math.random() * 10))
+        id = idArray.join('')
+    } while (!checkId(id))
+    return id
 }
 
-export default generateId;
+function checkId(contactId) {
+    const usedIds = Object.keys(localStorage) 
+    if (usedIds.includes(contactId)) {
+        console.log('ID conflict detected. Generating a new one...')
+        return false 
+    }
+    return true 
+}
+
+export default generateId
